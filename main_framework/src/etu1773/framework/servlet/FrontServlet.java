2315a3df044a4659b69e21a10cc8edc9f4f61ef7
JossElito179 <josselito.303@gmail.com>
2023-05-07 14:58:40 +0100

    sprint3 done

--- main_framework/src/etu1773/framework/servlet/FrontServlet.java
+++ main_framework/src/etu1773/framework/servlet/FrontServlet.java
@@ -1,11 +1,30 @@
 package etu1773.framework.servlet;
+
+import etu1773.framework.*;
+import java.util.*;
+import java.util.Map.Entry;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class FrontServlet extends HttpServlet {
 
     public HashMap<String,Mapping> MappingUrls;
+
+    public HashMap<String, Mapping> getMappingUrls() {
+        return MappingUrls;
+    }
+
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        MappingUrls = mappingUrls;
+    }
+
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -36,12 +55,28 @@ public class FrontServlet extends HttpServlet {
         processRequest(request, response);
     }
 
-    public void init() throws ServletException {
-        
+    public void init() throws Exception {
+        String path="C:/Users/LENOVO/projets/java_Framework/main_framework";
+        String packageName="src";
+        Utilitaire u =new Utilitaire();
+        ArrayList<Class> allclasses=u.findClassesInPackage(path,packageName);
+        setMappingUrls(u.findAllAnnotatedMethods(allclasses));
     }
 
     public void destroy() {
         
+    }
+
+    public static void main(String[] args) {
+        FrontServlet fs =new FrontServlet();
+        try {
+        fs.init();
+            for (Entry<String, Mapping> entry : fs.MappingUrls.entrySet()) {
+                System.out.println("Url : " + entry.getKey() + ", ClassName : " + entry.getValue().getClassName() +", MethodName :"+entry.getValue().getMethod() +"");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
