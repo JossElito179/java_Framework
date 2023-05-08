75c5045dabd939ae5810d1f1eabca5873135cd9f
JossElito179 <josselito.303@gmail.com>
2023-05-08 19:29:43 +0100

    sprint3 done

--- main_framework/src/etu1773/framework/servlet/FrontServlet.java
+++ main_framework/src/etu1773/framework/servlet/FrontServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class FrontServlet extends HttpServlet {
 
     public HashMap<String,Mapping> MappingUrls;
